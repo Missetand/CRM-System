7e06817ced5fab30e83b052bea78c440d185ef2a
Missetand <c.nielsen39@gmail.com>
2017-09-05 10:31:51 +0200

    sign-up working

--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    firebaseApp: Ember.inject.service(),
+    
+      actions: {
+        signUp() {
+          var email = this.controller.get('email');
+          var pass = this.controller.get('password');
+          const auth = this.get('firebaseApp').auth();
+          auth.createUserWithEmailAndPassword(email, pass).then((userResponse) => {
+            const user = this.store.createRecord('user', {
+              id: userResponse.uid,
+              email: userResponse.email
+            });
+            user.save().then(() => {
+              this.transitionTo('welcome');
+            }).catch(err => console.warn('was not able to save: ', err));
+          });
+        }
+      }
 });
