f65032efa972849d7b5513e1765701a1dcc029b3
Missetand <c.nielsen39@gmail.com>
2017-09-06 13:29:43 +0200

    sort and filter beta

--- app/components/municipality/municipality-table.js
+++ app/components/municipality/municipality-table.js
@@ -4,12 +4,20 @@ export default Ember.Component.extend({
 
   tagName: 'tr',
   classNames: ['table', 'table-bordered', 'table-hover'],
-  sortedTables: Ember.computed.sort('model', 'sortDefinition'),
-  sortBy: 'firmName',
-  reverseSort: false,
-  sortDefinition: Ember.computed.sort('reverseSort', function() {
-    let sortOrder = this.get('reverseSort') ? 'desc' : 'asc';
-    return [ `${this.get('sortBy')}:${sortOrder}` ];
-  })
+  municipality: [],
+  sortKeys: ['firmName'],
+  filter: '',
 
+  sortedMunicipality: Ember.computed.sort('model', 'sortKeys'),
+
+  filteredMunicipality: Ember.computed.filter('sortedMunicipality', function(municipality) {
+    var firmName = municipality.get('firmName').toLowerCase();
+    return firmName.indexOf(this.get('filter')) !== -1;
+  }).property('sortedMunicipality', 'filter'),
+
+  actions: {
+    sortBy: function(sort) {
+      this.set('sortKeys', [sort]);
+    }
+  }
 });
